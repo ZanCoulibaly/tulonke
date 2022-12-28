1e95cb2a16ee4f071d837f15ae1da35630ca3ce2
Zan Coulibaly <balamine129@gmail.com>
2022-12-28 16:11:14 +0000

    reseaux

--- js/highscores.js
+++ js/highscores.js
@@ -13,6 +13,19 @@ deleteScore = () => {
     window.location.reload();
 }
 
+document.querySelector('.share_twitter').addEventListener('click', function(e) {
+    e.preventDefault();
+    var url = this.getAttribute('data-url');
+    var shareUrl = "https://twitter.com/intent/tweet/?text=" + "Participez au jeux de Orange Mali via" + "&url=" + encodeURIComponent(url);
+    window.open(shareUrl, "Partagez");
+});
+document.querySelector('.share_facebook').addEventListener('click', function(e) {
+    e.preventDefault();
+    var url = this.getAttribute('data-url');
+    var shareUrl = "https://facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url);
+    window.open(shareUrl, "Partagez");
+});
+
 document.querySelector('body').addEventListener('contextmenu', disableRightClick);
 // document.querySelector('img').addEventListener('contextmenu', disableRightClick);
 document.addEventListener('contextmenu', disableRightClick);
