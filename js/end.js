51cc1134edc966a04076eb409aee1dd9e4a4f1cb
Zan Coulibaly <balamine129@gmail.com>
2022-12-30 11:34:22 +0000

    firebase

--- js/end.js
+++ js/end.js
@@ -4,6 +4,7 @@ const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const jeux = [];
 
 const MAX_HIGH_SCORES = 5;
 
@@ -15,17 +16,34 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = (e) => {
     e.preventDefault();
-
-    const score = {
+    const id = uuidv4();
+    const dfa = {
         score: mostRecentScore,
         name: username.value,
+        id: id
     };
-    highScores.push(score);
-    highScores.sort((a, b) => b.score - a.score);
-    highScores.splice(5);
-    localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/highscores.html');
-};
+    db.collection('jeux').doc(id).set(dfa).then(() => {
+        // console.log('Todo added successfully!');
+        $('.new-todo').val('');
+        jeux.push(dfa);
+        highScores.push(dfa);
+        highScores.sort((a, b) => b.dfa - a.dfa);
+        highScores.splice(5);
+        localStorage.setItem('highScores', JSON.stringify(highScores));
+        window.location.assign('/highscores.html');
+    }).catch(error => {
+        console.log('Erreur de registre base', error);
+    })
+
+}
+
+
+
+
+
+
+
+
 
 document.querySelector('body').addEventListener('contextmenu', disableRightClick);
 // document.querySelector('img').addEventListener('contextmenu', disableRightClick);
